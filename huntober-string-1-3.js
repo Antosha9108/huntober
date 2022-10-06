4e974c0e4a2a4ed73547c4f9adce8a9348112792
Antosha9108 <shelkovnikov9108@gmail.com>
2022-10-06 14:17:59 -0400

    add 1-3 kata

---
+++ huntober-string-1-3.js
@@ -0,0 +1,5 @@
+function replacerDayThree(str){
+    return str.replaceAll('s',' ').replaceAll('p',' ').replaceAll('a',' ').replaceAll('c',' ').replaceAll('e',' ')
+}
+
+console.log(replacerDayThree('space_kittens_and_puppies'))
